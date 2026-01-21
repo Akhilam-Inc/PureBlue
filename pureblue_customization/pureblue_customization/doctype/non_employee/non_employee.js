cfa07d5869df513b79b78f39e2b9ec6be295de8a
thakorvivek <vivekthakor1690@gmail.com>
2026-01-21 11:58:16 +0500

    doctype added

---
+++ pureblue_customization/pureblue_customization/doctype/non_employee/non_employee.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Akhilam Inc. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Non Employee", {
+// 	refresh(frm) {
+
+// 	},
+// });
